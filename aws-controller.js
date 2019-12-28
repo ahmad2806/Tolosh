c86b7bd0530a7981c97a771b7fa55964624fad7d
Mustafa Abudalu <mustaabu@amdocs.com>
2019-12-28 13:29:29 +0200

    added aws

---
+++ aws-controller.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const aws = require('aws-sdk');
+var secrets = require('./secrets');
+
+const s3 = new aws.S3({
+    accessKeyId: secrets.aws_key,
+    secretAccessKey: secrets.aws_secret,
+    signatureVersion: 'v4',
+    region:'eu-west-3'
+})
+
+exports.signedRequest = function(req, res) {
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+        Bucket: secrets.aws_bucket,
+        Key: fileName,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'private'
+    }
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+        if(err) {
+            console.log(err);
+            return res.end();
+        }
+        const returnData = {
+            signedRequest: data,
+            url: `https://${secrets.aws_bucket}.s3.amazonaws.com/${fileName}`
+        }
+        return res.json(returnData);
+    });
+};
+exports.getFileSignedRequest = function(req, res) {
+    const s3Params = {
+        Bucket: secrets.aws_bucket,
+        Key: req.params.fileName,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'private'
+    }
+
+    s3.getSignedUrl('getObject', s3Params, (err,data) => {
+        if(err) {
+            console.log(err);
+            return res.end();
+        }
+        return res.json(data);
+    })
+    
+};
+exports.listFiles = function(req, res) {
+    
+    const s3Params = {
+        Bucket: secrets.aws_bucket,
+        Delimiter: '/'
+    };
+
+    s3.listObjects(s3Params, (err, data)=> {
+        if(err) {
+            console.log(err);
+            return res.end();
+        }
+        return res.json(data);
+    });
+};
+exports.deleteFile = function(req, res) {
+    const s3Params = {
+        Bucket: secrets.aws_bucket,
+        Key: req.params.fileName
+    }
+    s3.deleteObject(s3Params, (err, data) => {
+        if (err) {
+            return res.end();
+        }
+        return res.status(200).send({'msg': 'File deleted'});
+    })
+    
+};
---
+++ secrets.js
@@ -0,0 +1,5 @@
+module.exports = {
+    aws_bucket: process.env.S3_BUCKET,
+    aws_key: process.env.AWS_ACCESS_KEY_ID,
+    aws_secret: process.env.AWS_SECRECT_ACCESS_KEY
+};
